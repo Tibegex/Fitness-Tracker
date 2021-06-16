25cf5bcb104402a6beb5b28984d5d6c9ed2a27ef
Tibegex <joshua_b_lee@yahoo.com>
2021-06-15 23:48:55 -0600

    Set up server.js file

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const logger = require("morgan");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+  useNewUrlParser: true,
+});
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}..`);
+});
